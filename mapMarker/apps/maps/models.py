986d1d669e3982594160e793c937f449167f248b
Alan Beck <beckalan303@gmail.com>
2020-10-27 14:40:08 -0300

    add a new index

--- mapMarker/apps/maps/models.py
+++ mapMarker/apps/maps/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Mark(models.Model):
+
+    name = models.CharField(max_length=300, null=True, blank=True)
+    estado = models.BooleanField(default=True)
+    latitud = models.CharField(max_length=9000, null=False, blank=False)
+    longitud = models.CharField(max_length=9000, null=False, blank=False)
